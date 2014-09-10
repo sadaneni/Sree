3c5d4de3ca81823c8cce781533b8990b9b7b1676
Sadaneni <ramsadaneni@gmail.com>
2014-09-10 22:48:00 +0100

    Added methods into regressiontestsuite by ramana

--- src/test/java/RegressionTestSuite.java
+++ src/test/java/RegressionTestSuite.java
@@ -1,5 +1,20 @@
+import org.junit.After;
+import org.junit.Before;
+
 /**
  * Created by VAMSHI on 10/09/2014.
  */
 public class RegressionTestSuite {
+    //This is my first
+
+    @Before
+    public void login() {
+
+    }
+
+    @After
+    public void logout()
+    {
+
+}
 }
